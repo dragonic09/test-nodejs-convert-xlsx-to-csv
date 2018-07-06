b300fb4cbaf6014649e051bbd9cca334b4dfaa8b
Thanakrid Kunghair <thanakrid@bluebik.com>
2018-07-06 17:18:20 +0700

    add more columns in csv and change file name

--- index.js
+++ index.js
@@ -15,8 +15,16 @@ let header = [
     "title",
     "name",
     "phone_number",
-    "start",
-    "valid_through"]
+    "start_date",
+    "valid_through",
+    "start_time",
+    "end_time",
+    "can_concurrent_parking",
+    "id",
+    "parking_lot_id",
+    "status",
+    "created_date",
+    "updated_date"]
 sheet.push(header);
 for(row = 1; row <= rowNum; row++) {
     let alphabet = "A";
@@ -32,13 +40,27 @@ for(row = 1; row <= rowNum; row++) {
             value = moment().startOf("day").format(dateFormat);
         } else if(column === 5) {
             value = value + " " + excel.Sheets.Sheet1[incrementAlphabet(alphabet) + row]["w"];
-        } 
+        } else if( column === 7) {
+            value = trimWhiteSpace(value);
+            value = removeWhiteSpace(value);
+        } else if( column === 1) {
+            value = trimWhiteSpace(value);
+            value = removeWhiteSpace(value);
+        }
 
         if(column !== 6) {
             rowData.push(value);
         }
         alphabet = incrementAlphabet(alphabet);
     }
+    rowData.push(moment().startOf("day").format("HH:mm:ss"));
+    rowData.push(moment().startOf("day").format("HH:mm:ss"));
+    rowData.push("false");
+    rowData.push(row);
+    rowData.push(1);
+    rowData.push("ACTIVE");
+    rowData.push(moment().startOf("day").format(dateFormat));
+    rowData.push(moment().startOf("day").format(dateFormat));
     sheet.push(rowData);
 }
 
@@ -50,8 +72,8 @@ xlsx.utils.book_append_sheet(workBook,csvBuild, "subscribers csv")
 console.log(workBook);
 
 //xlsx.writeFile(workBook, 'subscribers.csv');
-fs.writeFile(__dirname + "/test.csv", workBook.Sheets['subscribers csv'], function (err) {
-
+fs.writeFile(__dirname + "/subscribers.csv", workBook.Sheets['subscribers csv'], function (err) {
+    console.log(err);
 });
 
 function incrementAlphabet(c) {
@@ -60,6 +82,13 @@ function incrementAlphabet(c) {
 
 function trimWhiteSpace(str) {
     return str !== undefined && str !== null ? str.trim(): str;
+}
+
+function removeWhiteSpace(str) {
+    if(str.indexOf(' ') >=0)
+        return str.replace(/\s/g, '');
+    else
+        return str;
 }
 console.log(Object.keys(excel.Sheets.Sheet1).length);
 
